27f381509988fac2af042be612841a1081b1b773
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-06-08 13:31:38 +0200

    first iteration. added userinput basic. Not yet while loop implemented

--- src/input.js
+++ src/input.js
@@ -1,5 +1,27 @@
 // Create the Input Interface in Command line
+import { existsSync } from "fs";
 import { Contact } from "./crud.js";
+import readline from "node:readline";
 
-export function uiCli(){
+export class UiCli {
+	constructor(database) {
+		this.database = database;
+        this.alive = true;
+	}
+    
+    _createInterface(){
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+          });
+        return rl
+    }
+
+    userInput(){
+        let rl = this._createInterface()
+        rl.question(`User Input? `, answer => {
+            console.log(`This is user Input: ${answer}!`);
+            rl.close()
+        });
+    }
 }
