5560b281085c1b0af0bd267154d9d94b9ce19b1d
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-06-10 06:35:21 +0200

    added Switch Case : choiss

--- src/input.js
+++ src/input.js
@@ -8,7 +8,8 @@ export class UiCli {
 		this.database = database;
 	}
 
-	_createInterface() { //initialise instance
+	_createInterface() {
+		//initialise instance
 		const rl = readline.createInterface({
 			input: process.stdin,
 			output: process.stdout,
@@ -16,11 +17,40 @@ export class UiCli {
 		return rl;
 	}
 
+	_updateContact(answer) {}
+
+	_deleteContact(answer) {}
+
+	_listContact(answer) {}
+
 	userInput() {
 		let rl = this._createInterface();
-		rl.question(`User Input? `, (answer) => {
-			console.log(`This is user Input: ${answer}!`);
-			rl.close();
-		});
+		rl.question(
+			`-------------\nWhat Would you like to do?\n- Create (C)\n- Update (U)\n- Delete (D)\n- List (L)\n- List all (A)\n>> `,
+			(answer) => {
+				answer = answer.toUpperCase();
+				switch (answer) {
+					case "C":
+						console.log(`${answer} is the right one`);
+						break;
+					case "U":
+						console.log(`${answer} is the right one`);
+						break;
+					case "D":
+						console.log(`${answer} is the right one`);
+						break;
+					case "L":
+						console.log(`${answer} is the right one`);
+						break;
+					case "A":
+						console.log(`${answer} is the right one`);
+						break;
+					default:
+						console.log(`${answer} is not a valid one`);
+						break;
+				}
+				rl.close();
+			}
+		);
 	}
 }
