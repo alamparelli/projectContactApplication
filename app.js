f2afcb522b805faf3811b1d28b2ca9f1420fe93c
Alessandro LAMPARELLI <alessandro@lamparelli.eu>
2024-06-24 16:15:57 +0200

    app.js prettified

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import "dotenv/config";
-import { UiCli } from "./src/input.js";
-import { existsSync, writeFile } from "fs";
-import { dbExist } from "./src/connectDb.js";
+import 'dotenv/config';
+import { UiCli } from './src/input.js';
+import { existsSync, writeFile } from 'fs';
+import { dbExist } from './src/connectDb.js';
 
 const localDb = process.env.LOCAL_JSONFILE;
 const app = new UiCli(localDb);
 
-console.clear()
-dbExist()
+console.clear();
+dbExist();
 app.userInput();
